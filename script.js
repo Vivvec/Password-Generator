232064e6d74d301fd617c1a14ddaccaed7027623
Hermes <danashepherd757@gmail.com>
2021-03-06 02:57:45 -0600

    created variables for each character option

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Assignment code here
-var length = prompt("Please choose password length. Must be between 8 and 128 characters long.");
 
-if (length != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
-  console.log(length);
-}
+//First prompt, Length
+var length = Number(prompt("Please choose password length. Must be between 8 and 128 characters long."));
+
+//Second prompt, Numeric
+var numeric = prompt("Would you like to include numeric characters?");
+
+//Third Uppercase
+var uppercase = prompt("Would you like to include uppercase characters?");
+
+//Fourth lowercase
+var lowercase = prompt("Would you like to include lowercase characters?");
+
+//Fifth special
+var special = prompt("Would you like to include special characters?")
 
 
 // Get references to the #generate element
@@ -19,6 +27,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
